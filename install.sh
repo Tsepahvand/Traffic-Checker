db1b4ed35e2d0b5c9357d3c121ad1fd87e9943dc
Taha Sepahvand <realtaha.sepahvand@gmail.com>
2024-12-01 15:49:26 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 GREEN="\033[0;32m"
 RED="\033[0;31m"
 YELLOW="\033[0;33m"
-NC="\033[0m"
+NC="\033[0m" 
 
 print_success() {
     echo -e "${GREEN}$1${NC}"
@@ -53,28 +53,20 @@ else
     print_warning "No requirements.txt found, skipping dependency installation."
 fi
 
-read -p "What is your panel type Sanaei or Alireza? (s/a): " panel_type
-
-if [[ "$panel_type" == "s" || "$panel_type" == "S" ]]; then
-    print_success "Panel type is Sanaei."
-elif [[ "$panel_type" == "a" || "$panel_type" == "A" ]]; then
-    print_success "Panel type is Alireza. Modifying bot.py..."
-    sed -i 's|get_client_endpoint = "/panel/api/inbounds/getClientTraffics/"|get_client_endpoint = "/xui/API/inbounds/getClientTraffics/"|g' bot.py
-    print_success "Line 262 in bot.py has been modified."
-else
-    print_error "Invalid panel type entered. Exiting."
-    exit 1
-fi
 
 read -p "Do you want to convert client names to uppercase (yes/no)? " response
+
 
 if [[ "$response" == "yes" || "$response" == "y" ]]; then
     print_success "Client names will be converted to uppercase."
 else
     print_warning "Client names will not be converted to uppercase. Modifying bot.py..."
+
+
     sed -i 's/client_name = update.message.text.strip().upper()/client_name = update.message.text.strip()/g' bot.py
     print_success "Line 250 in bot.py has been modified."
 fi
+
 
 loading_animation "Checking if Docker is installed"
 if ! [ -x "$(command -v docker)" ]; then
@@ -85,6 +77,7 @@ else
     print_success "Docker is already installed."
 fi
 
+
 loading_animation "Checking if Docker Compose is installed"
 if ! [ -x "$(command -v docker-compose)" ]; then
     print_warning "Docker Compose is not installed. Installing..."
@@ -93,6 +86,7 @@ else
     print_success "Docker Compose is already installed."
 fi
 
+
 loading_animation "Checking if SQLite is installed"
 if ! [ -x "$(command -v sqlite3)" ]; then
     print_warning "SQLite is not installed. Installing..."
@@ -100,6 +94,7 @@ if ! [ -x "$(command -v sqlite3)" ]; then
 else
     print_success "SQLite is already installed."
 fi
+
 
 DB_PATH="detail.db"
 if [ ! -f "$DB_PATH" ]; then
@@ -115,35 +110,22 @@ else
     print_success "Database $DB_PATH already exists."
 fi
 
+
 read -p "Enter the bot token: " bot_token
 read -p "Enter the owner ID: " owner_id
+
 
 if [[ -z "$bot_token" || -z "$owner_id" || ! "$owner_id" =~ ^[0-9]+$ ]]; then
     print_error "Invalid input. Please ensure the bot token and owner ID are correct."
     exit 1
 fi
 
+
 sqlite3 "$DB_PATH" <<EOF
 INSERT INTO settings (token, owner_id) VALUES ('$bot_token', $owner_id);
 EOF
 print_success "Information saved."
 
+
 loading_animation "Running the bot with Docker Compose"
 docker-compose up --build -d || { print_error "Docker Compose failed to start."; exit 1; }
-
-read -p "Do you want to set a specific port for WebUI? (y/n): " set_port
-
-if [[ "$set_port" == "y" || "$set_port" == "Y" ]]; then
-    read -p "Please enter the port number: " custom_port
-    print_success "Changing the port in webUI.py to $custom_port..."
-    sed -i "s|app.run(debug=True, host='0.0.0.0', port=5000)|app.run(debug=True, host='0.0.0.0', port=$custom_port)|g" webUI.py
-    print_success "Port changed successfully in webUI.py."
-else
-    print_warning "Skipping port customization for WebUI."
-fi
-
-print_success "Running WebUI and Uvicorn..."
-nohup python3 webUI.py &  
-nohup uvicorn app:app --port 7386 &
-
-print_success "Setup complete!"
