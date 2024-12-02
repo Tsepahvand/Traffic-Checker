b8e36fcc1ed9c8aae3242d126473499fcfc3a0b0
Taha Sepahvand <realtaha.sepahvand@gmail.com>
2024-12-02 11:12:51 -0800

    Update webUI.py

--- webUI.py
+++ webUI.py
@@ -134,5 +134,5 @@ def index(email_id):
         return render_template('error.html', lang=lang, translations=translations[lang]), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True,host="0.0.0.0", port=5000)
 
